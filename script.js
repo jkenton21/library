bda309e82d729801d78d5bad072816a74b6bb68f
Jared <jkenton21@live.com>
2022-02-20 12:13:34 -0600

    basic skeleton added for html, css, and js files

---
+++ script.js
@@ -0,0 +1,13 @@
+// Library main JavaScript file for the actual execution of the application
+// Will create book cards based on the form data entered to be displayed on the content seciton of the page
+
+//crate array to store the books
+let myLibrary = [];
+
+function Book() {
+    //function for creating a book
+}
+
+function addBookToLibrary() {
+    //function that adds the book to the library array
+}
